e919d6aa5642724c443f74a84ab2cc2e4876aa63
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2020-12-08 22:22:10 +0100

    Sketch very simple Git overview

--- lw-git.lisp
+++ lw-git.lisp
@@ -1,3 +1,24 @@
 ;;;; lw-git.lisp
 
 (in-package #:lw-git)
+
+(defclass ui-base (capi:interface)
+  ((path :initarg :path
+         :reader path
+         :type string ; shall this be a directory instead?
+         :documentation
+         "The path provides the context for the git action.")))
+
+(defclass display-pane-horizontal (capi:display-pane)
+  ()
+  (:default-initargs
+   :title-position :left
+   :background :transparent))
+
+(capi:define-interface ui-overview (ui-base)
+  ()
+  (:panes
+   (head-pane display-pane-horizontal :title "Head" :text "This is the head.")
+   (merge-pane display-pane-horizontal :title "Merge" :text "This is the merge.")
+   (tags-pane display-pane-horizontal :title "Tags" :text "These are the tags."))
+  (:layouts (main capi:column-layout '(head-pane merge-pane tags-pane))))
